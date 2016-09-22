eb0182a565c364f951a4b10e6aec5a42bdce0b08
Jennifer Swartz <swartzjenn@gmail.com>
2016-09-21 22:23:35 -0400

    Refactor display of scores when computer goes first

--- programming_foundations/lesson_4/tictactoe.rb
+++ programming_foundations/lesson_4/tictactoe.rb
@@ -6,9 +6,9 @@ COMPUTER_MARKER = 'O'.freeze
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]].freeze       # diagonals
-PLAYER_WHO_GOES_FIRST = 'choose'
-PLAYER = 'player'
-COMPUTER = 'computer'
+PLAYER_WHO_GOES_FIRST = 'choose'.freeze
+PLAYER = 'Player'.freeze
+COMPUTER = 'Computer'.freeze
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -16,7 +16,7 @@ end
 
 # rubocop:disable Metrics/AbcSize
 def display_board(brd)
-  # system 'clear'
+#  system 'clear'
   puts ""
   puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
   puts "     |     |"
@@ -144,9 +144,8 @@ computer_score = 0
 player_score = 0
 first_player = nil
 
-
 prompt "Welcome to Tic Tac Toe!"
-prompt "The first to score five wins the round!"
+prompt "The first to score five wins!"
 
 loop do
   if PLAYER_WHO_GOES_FIRST == 'choose'
@@ -168,14 +167,15 @@ loop do
   loop do
     board = initialize_board
 
-    #  If set at player goes first or player is select, player goes first
+    #  If PLAYER_WHO_GOES_FIRST is player, set current_player variable to PLAYER
+    #  and display board
     if PLAYER_WHO_GOES_FIRST == PLAYER || first_player == PLAYER
       current_player = PLAYER
-    elsif
-      current_player = COMPUTER
-    end
+      display_board(board)
 
-    display_board(board)
+    #  Otherwise set current_player variable to computer
+    elsif current_player = COMPUTER
+    end
 
     loop do
       place_piece!(board, current_player)
@@ -186,22 +186,21 @@ loop do
 
     display_board(board)
 
-    if someone_won?(board)
-      prompt "#{detect_winner(board)} won this round!"
-    else
-      prompt "It's a tie!"
-    end
-
     if detect_winner(board) == PLAYER
       player_score += 1
     elsif detect_winner(board) == COMPUTER
       computer_score += 1
     end
 
-    display_score(player_score, computer_score)
+    if someone_won?(board)
+      prompt "#{detect_winner(board)} won this round!"
+      display_score(player_score, computer_score)
+    else
+      prompt "It's a tie!"
+    end
 
     if player_score == 5 || computer_score == 5
-      prompt "#{detect_winner(board)} scored five points and wins the round!"
+      prompt "#{detect_winner(board)} scored five points and wins!"
       break
     end
   end
