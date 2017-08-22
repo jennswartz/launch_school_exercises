c40722bd3a76d1267fed0ef292024f334492f0c1
Jennifer Swartz <swartzjenn@gmail.com>
2017-08-22 13:39:16 -0400

    Redo Loops 1 for practice

---
+++ exercises/ruby_basics/loops_1_x2/control_the_loop.rb
@@ -0,0 +1,6 @@
+iterations = 1
+
+while iterations <= 5
+  puts "Number of iterations = #{iterations}"
+  iterations += 1
+end
---
+++ exercises/ruby_basics/loops_1_x2/print_until.rb
@@ -0,0 +1,8 @@
+numbers = [7, 9, 13, 25, 18]
+counter = 0
+length = numbers.length
+
+until counter == length
+  puts numbers[counter]
+  counter += 1
+end
